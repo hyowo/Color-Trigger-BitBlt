d50abc6db05f5f94a8c9813473e1156399c8e93a
hyowo <vilumaaerik@gmail.com>
2023-06-09 12:03:13 +0300

    initial commit

---
+++ src/color_filtering.rs
@@ -0,0 +1,88 @@
+pub fn rgb_to_hsv(r: u8, g: u8, b: u8) -> (f32, f32, f32) {
+    let f_r = r as f32 / 255.0;
+    let f_g = g as f32 / 255.0;
+    let f_b = b as f32 / 255.0;
+
+    let f_c_max = f_r.max(f_g).max(f_b);
+    let f_c_min = f_r.min(f_g).min(f_b);
+    let f_delta = f_c_max - f_c_min;
+
+    let mut f_h = 0.0;
+    let mut f_s = 0.0;
+    let f_v = f_c_max;
+
+    if f_delta > 0.0 {
+        if f_c_max == f_r {
+            f_h = 60.0 * ((f_g - f_b) / f_delta).rem_euclid(6.0);
+        } else if f_c_max == f_g {
+            f_h = 60.0 * ((f_b - f_r) / f_delta + 2.0);
+        } else if f_c_max == f_b {
+            f_h = 60.0 * ((f_r - f_g) / f_delta + 4.0);
+        }
+
+        if f_c_max > 0.0 {
+            f_s = f_delta / f_c_max;
+        }
+    }
+
+    (f_h, f_s, f_v)
+}
+
+pub fn threshold_white(image_data: &[u8], target_color: (f32, f32, f32)) -> Vec<u8> {
+    let mut new_image_data = Vec::with_capacity(image_data.len() / 4);
+    let mut index = 0;
+
+    while index + 4 <= image_data.len() {
+        let (h, s, v) = rgb_to_hsv(image_data[index+2], image_data[index+1], image_data[index]);
+        let similarity = calculate_hsv_similarity(target_color, (h,s,v), 2.0);
+        new_image_data.push((similarity * 255.0) as u8);
+
+        index += 4;
+    }
+
+    new_image_data
+}
+
+pub fn calculate_hsv_similarity(hsv1: (f32, f32, f32), hsv2: (f32, f32, f32), max_h_diff: f32) -> f32 {
+    let (h1, s1, v1) = hsv1;
+    let (h2, s2, v2) = hsv2;
+
+    let dh = (h1 - h2).abs();
+    let ds = (s1 - s2).abs();
+    let dv = (v1 - v2).abs();
+
+    // Adjust the hue difference for cyclic nature of hue values
+    let hue_difference = if dh > 180.0 { 360.0 - dh } else { dh };
+
+    if hue_difference <= max_h_diff {
+        // Calculate the Euclidean distance between HSV values
+        let distance = (hue_difference.powi(2) + ds.powi(2) + dv.powi(2)).sqrt();
+        return 1.0 - (distance / (3.0 * 100.0_f32.sqrt()));
+    }
+
+    0.0
+}
+
+pub fn find_head_point(image: &[u8]) -> Option<(usize, usize)> {
+    let image_size = (image.len() as f64).sqrt() as usize;
+    
+    // Find highest_y
+    let highest_y = (0..image_size/2)
+        .rev()
+        .find(|&row| image[row * image_size + 50] > 0)?;
+
+    // Scan left to find a value > 0
+    let left_point = (0..image_size/2)
+        .rev()
+        .find(|&col| image[(highest_y + 1) * image_size + col] > 0)?;
+
+    // Scan right to find a value > 0
+    let right_point = (image_size/2+1..image_size)
+        .find(|&col| image[(highest_y + 1) * image_size + col] > 0)?;
+
+    // Calculate the middle x coordinate
+    let middle_x = (left_point + right_point) / 2;
+
+    // Return the result
+    Some((middle_x, highest_y + 1))
+}
---
+++ src/main.rs
@@ -0,0 +1,198 @@
+mod color_filtering;
+
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+use std::thread;
+use std::time::{Duration, Instant};
+use std::{env, mem, ptr};
+use winapi::shared::minwindef::LPVOID;
+use winapi::shared::windef::{HGDIOBJ, POINT};
+use winapi::um::wingdi::{
+    BitBlt, CreateCompatibleBitmap, CreateCompatibleDC, GetDIBits, SelectObject, BITMAPINFOHEADER,
+    BI_RGB, DIB_RGB_COLORS, SRCCOPY,
+};
+use winapi::um::winuser::{
+    GetCursorPos, GetDC, GetKeyState, GetSystemMetrics, SendInput, INPUT, INPUT_MOUSE,
+    MOUSEEVENTF_LEFTDOWN, MOUSEEVENTF_LEFTUP, SM_CXSCREEN, SM_CYSCREEN, VK_SHIFT,
+};
+
+use crate::color_filtering::{find_head_point, threshold_white};
+
+const YELLOW_TARGET: (f32, f32, f32) = (60.0, 1.0, 1.0);
+const PURPLE_TARGET: (f32, f32, f32) = (300.0, 1.0, 1.0);
+const RED_TARGET: (f32, f32, f32) = (360.0, 1.0, 1.0);
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let binding = String::from("Yellow");
+    let player_color = args.get(1).unwrap_or(&binding);
+    let scan_size = args.get(2).and_then(|arg| arg.parse().ok()).unwrap_or(100);
+    let tb_delay = args.get(3).and_then(|arg| arg.parse().ok()).unwrap_or(350);
+    let refresh_rate = args.get(4).and_then(|arg| arg.parse().ok()).unwrap_or(60);
+
+    let target = match player_color.as_str() {
+        "Purple" => PURPLE_TARGET,
+        "Red" => RED_TARGET,
+        _ => YELLOW_TARGET,
+    };
+
+    let _ = thread::spawn(move || {
+        let screen_width = unsafe { GetSystemMetrics(SM_CXSCREEN) };
+        let screen_height = unsafe { GetSystemMetrics(SM_CYSCREEN) };
+
+        let h_screen = unsafe { GetDC(ptr::null_mut()) };
+        let h_bitmap = unsafe { CreateCompatibleBitmap(h_screen, scan_size, scan_size) };
+        let mut screen_data = vec![0u8; (scan_size * scan_size * 4) as usize];
+        let h_dc = unsafe { CreateCompatibleDC(h_screen) };
+
+        let middle_screen = POINT {
+            x: screen_width / 2,
+            y: screen_height / 2,
+        };
+
+        let mut bmi: BITMAPINFOHEADER = unsafe { mem::zeroed() };
+        bmi.biSize = std::mem::size_of::<BITMAPINFOHEADER>() as u32;
+        bmi.biPlanes = 1;
+        bmi.biBitCount = 32;
+        bmi.biWidth = scan_size;
+        bmi.biHeight = -scan_size;
+        bmi.biCompression = BI_RGB;
+        bmi.biSizeImage = 0;
+
+        let mut previous_data: Option<Vec<u8>> = None;
+        let mut last_frame_time = Instant::now();
+        let mut tb_cd = Instant::now();
+
+        loop {
+            while !is_shift_key_pressed() || !is_movement_keys_pressed() || tb_cd.elapsed() < Duration::from_millis(tb_delay) {
+                thread::yield_now();
+            }
+
+            // Wait for next VBI
+            let vbi_duration = get_vertical_blank_duration(refresh_rate);
+            let elapsed = last_frame_time.elapsed();
+            if elapsed < vbi_duration {
+                thread::sleep(vbi_duration - elapsed);
+            }
+
+            let old_obj = unsafe { SelectObject(h_dc, h_bitmap as HGDIOBJ) };
+            unsafe {
+                BitBlt(
+                    h_dc,
+                    0,
+                    0,
+                    scan_size,
+                    scan_size,
+                    h_screen,
+                    middle_screen.x - (scan_size / 2),
+                    middle_screen.y - (scan_size / 2),
+                    SRCCOPY,
+                )
+            };
+
+            unsafe { SelectObject(h_dc, old_obj) };
+            unsafe {
+                GetDIBits(
+                    h_dc,
+                    h_bitmap,
+                    0,
+                    scan_size.try_into().unwrap(),
+                    screen_data.as_mut_ptr() as LPVOID,
+                    &mut bmi as *mut _ as *mut _,
+                    DIB_RGB_COLORS,
+                )
+            };
+
+            let data = threshold_white(&mut screen_data, target);
+
+            if let Some(ref previous_frame) = previous_data {
+                if data == *previous_frame {
+                    continue;
+                }
+            }
+
+            previous_data = Some(data.clone());
+            last_frame_time = Instant::now();
+
+            if let Some(head_point) = find_head_point(&data) {
+                if head_point.0 > 47 && head_point.0 < 53 && head_point.1 < 50 && head_point.1 > 35 {
+                    send_left_click();
+                    tb_cd = Instant::now();
+                }
+            }
+        }
+    });
+
+    println!(
+        "Player Color: {}\nSearch Size: {}\nTriggerbot Delay: {}\nRefreshrate: {}",
+        player_color, scan_size, tb_delay, refresh_rate
+    );
+
+    let running = Arc::new(AtomicBool::new(true));
+
+    let running_signal = Arc::clone(&running);
+    ctrlc::set_handler(move || {
+        running_signal.store(false, Ordering::SeqCst);
+    })
+    .expect("Error setting Ctrl-C handler");
+
+    while running.load(Ordering::SeqCst) {
+        thread::sleep(Duration::from_secs(1));
+    }
+}
+
+fn get_vertical_blank_duration(refresh_rate: u32) -> Duration {
+    let frame_duration = Duration::from_secs(1) / refresh_rate;
+    let vbi_duration = frame_duration / 2;
+
+    vbi_duration - Duration::from_micros(100)
+}
+
+pub fn convert_to_rgba(data: Vec<u8>) -> Vec<u8> {
+    let mut rgba_data = Vec::with_capacity(data.len() * 4);
+
+    for value in data {
+        rgba_data.push(value);
+        rgba_data.push(value);
+        rgba_data.push(value);
+        rgba_data.push(0);
+    }
+
+    rgba_data
+}
+
+fn is_movement_keys_pressed() -> bool {
+    (unsafe { GetKeyState(b'A' as i32) } as i32 & 0x8000) == 0
+        && (unsafe { GetKeyState(b'W' as i32) } as i32 & 0x8000) == 0
+        && (unsafe { GetKeyState(b'D' as i32) } as i32 & 0x8000) == 0
+        && (unsafe { GetKeyState(b'S' as i32) } as i32 & 0x8000) == 0
+}
+
+fn is_shift_key_pressed() -> bool {
+    (unsafe { GetKeyState(VK_SHIFT) } as i32 & 0x8000) != 0
+}
+
+pub fn send_left_click() {
+    unsafe {
+        let mut cursor_pos: POINT = POINT { x: 0, y: 0 };
+        GetCursorPos(&mut cursor_pos);
+
+        let mut input: INPUT = std::mem::zeroed();
+        input.type_ = INPUT_MOUSE;
+        let mut flags = MOUSEEVENTF_LEFTDOWN;
+        input.u.mi_mut().dwFlags = flags;
+        SendInput(
+            1,
+            &mut input as *mut INPUT,
+            std::mem::size_of::<INPUT>() as i32,
+        );
+
+        flags = MOUSEEVENTF_LEFTUP;
+        input.u.mi_mut().dwFlags = flags;
+        SendInput(
+            1,
+            &mut input as *mut INPUT,
+            std::mem::size_of::<INPUT>() as i32,
+        );
+    }
+}
